84c603670bf4123c26f6325df55ed0c90e19c32c
Alexander Senko <Alexander.Senko@gmail.com>
2024-02-05 16:51:38 +0700

    Framework: use Memery for memoization

--- app/models/concerns/adjustable_schema/active_record/relationships.rb
+++ app/models/concerns/adjustable_schema/active_record/relationships.rb
@@ -1,12 +1,15 @@
+require 'memery'
+
 module AdjustableSchema
 	module ActiveRecord
 		concern :Relationships do
 			class_methods do
-				def relationships
-					@relationships ||= # cache
-							Config.association_directions.to_h do
-								[ _1, Relationship.abstract.send(Config.shortcuts.opposite[_1], self) ]
-							end
+				include Memery
+
+				memoize def relationships
+					Config.association_directions.to_h do
+						[ _1, Relationship.abstract.send(Config.shortcuts.opposite[_1], self) ]
+					end
 				end
 
 				def roles(&) = Role.of self, &
