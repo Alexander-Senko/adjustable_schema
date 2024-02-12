53c112b3b7a86cca6ba8881c16ce15f6010bbdf2
Alexander Senko <Alexander.Senko@gmail.com>
2024-02-12 14:32:57 +0700

    API: added an association for roleless children
    That could be useful when you need to fetch child records without a
    role.
    
    We've already had associations for each role, plus one for records of a
    class all together regardless of their roles. And, in case of
    self-related associations, we had a vacant name as well.
    
    The association seem to be an equivalent for `children.roleless`, but it
    provides a bit more in fact. Being a full-featured association it's
    writable and you can update it easily, e.g. via a REST API as the new
    name would match both read & write routes unlike the original
    `children`.
    
    Example:
    
    ```ruby
    AdjustableSchema::Relationship.seed! Message, roles: %w[translation]
    
    message = Message.first
    message.translations # translations
    message.children     # all children, incl. translations
    message.messages     # NEW! "ordinary" nested messages
    ```
    
    + Refactored the tests a bit.

--- test/lib/active_record/association_test.rb
+++ test/lib/active_record/association_test.rb
@@ -3,48 +3,67 @@ require 'minitest/autorun'
 
 require 'adjustable_schema/active_record/association'
 
-describe AdjustableSchema::ActiveRecord::Association do
-	let(:described_class) { AdjustableSchema::ActiveRecord::Association }
+module AdjustableSchema
+	describe ActiveRecord::Association do
+		let(:described_class) { ActiveRecord::Association }
 
-	subject { described_class.new Model1, direction, target, role }
+		subject { described_class.new owner, direction, target, role }
 
-	let(:direction) { :target }
-	let(:target)    { Model2 }
-	let(:role)      { AdjustableSchema::Relationship::Role.find_or_create_by! name: role_name }
-	let(:role_name) { :dummy_role }
-	let(:record)    { subject.owner.create! }
+		let(:direction) { :target }
+		let(:owner)     { Model1 }
+		let(:target)    { Model2 }
+		let(:role)      { Relationship::Role.find_or_create_by! name: role_name }
+		let(:role_name) { :dummy_role }
+		let(:record)    { subject.owner.create! }
 
-	describe '#define' do
-		let(:associated_with_role_method_name) { "#{subject.name}_with_roles" }
+		describe '#define' do
+			let(:associated_with_role_method_name) { "#{subject.name}_with_roles" }
 
-		before { subject.define }
+			before { subject.define }
 
-		it 'defines an association' do
-			_(subject.owner.reflect_on_association subject.name)
-					.wont_be_nil
-			_(subject.owner.create.send(subject.name).to_a)
-					.must_be_kind_of Array
-		end
-
-		describe 'with a role' do
-			it "doesn't define role methods" do
-				_(record).wont_respond_to associated_with_role_method_name
+			it 'defines an association' do
+				_(owner.reflect_on_association subject.name)
+						.wont_be_nil
+				_(record.send(subject.name).to_a)
+						.must_be_kind_of Array
 			end
-		end
 
-		describe 'without a role' do
-			let(:role) {}
+			describe 'with a role' do
+				it "doesn't define role methods" do
+					_(record).wont_respond_to associated_with_role_method_name
+				end
+			end
 
-			let(:associated_with_role) { record.send associated_with_role_method_name, role_name }
+			describe 'without a role' do
+				let(:role) {}
 
-			it 'defines #<association>_with_roles' do
-				_(record                    ).must_respond_to associated_with_role_method_name
-				_(associated_with_role      ).must_be_kind_of ActiveRecord::Relation
-				_(associated_with_role.klass).must_equal      target
+				let(:associated_with_role) { record.send associated_with_role_method_name, role_name }
 
-				associated_with_role
-						.distinct.pluck('adjustable_schema_relationship_roles.name')
-						.each { _(_1).must_equal role_name.to_s }
+				it 'defines #<association>_with_roles' do
+					_(record)
+							.must_respond_to associated_with_role_method_name
+					_(associated_with_role)
+							.must_be_kind_of ::ActiveRecord::Relation
+					_(associated_with_role.klass)
+							.must_equal target
+
+					associated_with_role
+							.distinct.pluck('adjustable_schema_relationship_roles.name')
+							.each { _(_1).must_equal role_name.to_s }
+				end
+
+				describe 'self-targeted' do
+					let(:target) { owner }
+
+					let(:roleless_children_name) { owner.table_name }
+
+					it 'defines an extra association for roleless children' do
+						_(owner.reflect_on_association roleless_children_name)
+								.wont_be_nil
+						_(record.send(roleless_children_name).to_a)
+								.must_be_kind_of Array
+					end
+				end
 			end
 		end
 	end
