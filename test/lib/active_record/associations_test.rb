0062d4489da45043f0a94f3801a663a87f2e7bcb
Alexander Senko <Alexander.Senko@gmail.com>
2024-02-12 10:44:11 +0700

    Tests: added DB seeds and some cleanup
    …to keep a tight rein on model associations.

--- test/lib/active_record/associations_test.rb
+++ test/lib/active_record/associations_test.rb
@@ -3,8 +3,6 @@ require 'minitest/autorun'
 
 describe AdjustableSchema::ActiveRecord::Associations do
 	it 'defines associations' do
-		AdjustableSchema::Relationship.seed! 'Model1', 'Model2'
-
 		_(Model1.new).must_respond_to :model2s
 		_(Model2.new).must_respond_to :model1s
 	end
