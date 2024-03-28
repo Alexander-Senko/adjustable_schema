4927668bda3c8b5aac997a603ab535bafab23d18
Alexander Senko <Alexander.Senko@gmail.com>
2024-03-28 21:50:59 +0700

    DB: improved schema constraints
    * `roles.name` is `NOT NULL`,
    * `UNIQUE` constraints should treat `NULLS` as `NOT DISTINCT`.

--- db/migrate/01_create_adjustable_schema_relationship_tables.rb
+++ db/migrate/01_create_adjustable_schema_relationship_tables.rb
@@ -4,7 +4,7 @@ class CreateAdjustableSchemaRelationshipTables < ActiveRecord::Migration[7.1]
 		primary_key_type, foreign_key_type = primary_and_foreign_key_types
 
 		create_table :adjustable_schema_relationship_roles do |t|
-			t.string :name, index: { unique: true }
+			t.string :name, null: false, index: { unique: true }
 
 			t.timestamps
 		end
@@ -17,13 +17,20 @@ class CreateAdjustableSchemaRelationshipTables < ActiveRecord::Migration[7.1]
 
 			t.timestamps
 
-			t.index %i[
+			%i[
 					source_id source_type
 					target_id target_type
 					role_id
 			].tap { |columns|
-					columns.reject! { _1.ends_with? '_type' } if foreign_key_type == :uuid # OPTIMIZATION: IDs are unique
-			}, unique: true, name: :index_adjustable_schema_relationships_uniqueness
+				columns.reject! { _1.ends_with? '_type' } if foreign_key_type == :uuid # OPTIMIZATION: IDs are unique
+
+				# NULLS are DISTINCT by default.
+				# One can use `ADD CONSTRAINT … UNIQUE NULLS NOT DISTINCT (…)` instead
+				t.index columns,
+						unique: true, where: 'role_id IS NOT NULL', name: :index_adjustable_schema_relationships_uniqueness_with_role
+				t.index columns.excluding(:role_id),
+						unique: true, where: 'role_id IS     NULL', name: :index_adjustable_schema_relationships_uniqueness_without_role
+			}
 		end
 	end
 
