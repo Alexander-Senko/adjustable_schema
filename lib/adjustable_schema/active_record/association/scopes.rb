e9cea50ea68f323f7a74d609e5235502a7ea0fbd
Alexander Senko <Alexander.Senko@gmail.com>
2025-03-10 02:22:09 +0700

    Refactoring: reduced code complexity
    Extracted some method code into separate methods.

--- lib/adjustable_schema/active_record/association/scopes.rb
+++ lib/adjustable_schema/active_record/association/scopes.rb
@@ -3,24 +3,7 @@ module AdjustableSchema
 		module Association::Scopes
 			module Recursive
 				def recursive
-					with_recursive(recursive_table.name => [
-							unscope(:order, :group, :having)
-									.select(
-											select_values = self.select_values.presence || arel_table[Arel.star],
-											Arel.sql('1').as('distance'),
-									),
-
-							unscoped
-									.select(
-											select_values,
-											(recursive_table[:distance] + 1).as('distance'),
-									)
-									.joins(inverse_association_name)
-									.joins(<<~SQL)
-										JOIN #{recursive_table.name}
-												ON #{recursive_table.name}.#{primary_key} = #{inverse_table.name}.#{primary_key}
-									SQL
-					])
+					with_recursive(recursive_table.name => [ recursion_base, recursive_step ])
 							.unscope(:select, :joins, :where)
 							.from(recursive_table.alias table_name)
 							.distinct
@@ -28,6 +11,25 @@ module AdjustableSchema
 				end
 
 				private
+
+				def recursion_base
+					unscope(:order, :group, :having)
+							.select(recursive_select_values,
+									Arel.sql('1').as('distance'),
+							)
+				end
+
+				def recursive_step
+					unscoped
+							.select(recursive_select_values,
+									(recursive_table[:distance] + 1).as('distance'),
+							)
+							.joins(inverse_association_name)
+							.joins(<<~SQL.squish)
+								JOIN #{recursive_table.name}
+									ON #{recursive_table.name}.#{primary_key} = #{inverse_table.name}.#{primary_key}
+							SQL
+				end
 
 				def association_name = @association.reflection.name
 
@@ -41,6 +43,8 @@ module AdjustableSchema
 
 				def recursive_table = Arel::Table.new [ :recursive, association_name, klass.table_name ] * '_'
 				def   inverse_table = Arel::Table.new [     inverse_association_name, klass.table_name ] * '_' # HACK: depends on ActiveRecord internals
+
+				def recursive_select_values = select_values.presence || arel_table[Arel.star]
 			end
 		end
 	end
